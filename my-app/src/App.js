87cd50bb67aaf9b95184f3f6c5bfc9f9ecba44ce
shiranka <shiran2962@gmail.com>
2020-04-06 16:10:33 +0300

    change folder to reducers from reducers and store

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import TasksList from './components/TasksList'
 import TaskForm from './components/TaskForm'
 import { Provider } from 'react-redux'
-import { store } from './redux/reducers_and_store/task_reducer'
+import { store } from './redux/reducers/task_reducer'
 
 function App() {
   return (
