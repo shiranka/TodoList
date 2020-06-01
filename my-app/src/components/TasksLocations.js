b1b8841457da38d52e3c3c121e8399a73dc73ffc
shiranka <shiran2962@gmail.com>
2020-06-01 10:37:08 +0300

    add one point to the map on another layer

--- my-app/src/components/TasksLocations.js
+++ my-app/src/components/TasksLocations.js
@@ -1,14 +1,16 @@
 import 'ol/ol.css'
-import Paper from '@material-ui/core/Paper'
+import Map from 'ol/Map'
+import View from 'ol/View'
+import Feature from 'ol/Feature'
+import Point from 'ol/geom/Point'
+import { transform } from 'ol/proj'
 import React, { useEffect } from 'react'
-import { defaults as defaultControls } from 'ol/control'
-import {transform} from 'ol/proj'
+import { useSelector } from 'react-redux'
+import Paper from '@material-ui/core/Paper'
 import { withStyles } from '@material-ui/core/styles'
-import { useDispatch, useSelector } from 'react-redux'
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import OSM from 'ol/source/OSM';
+import { OSM, Vector as VectoreSource } from 'ol/source'
+import { defaults as defaultControls } from 'ol/control'
+import { Tile as TileLayer, Vector as VectoreLayer }from 'ol/layer'
 
 const styles = {
   root: {
@@ -21,27 +23,35 @@ const styles = {
 }
 
 const TasksLocations = ({ classes }) => {
-  const dispatch = useDispatch() 
   const tasks = useSelector((state) => state.tasks)
   const isHideTasks = useSelector((state) => state.isHideTasksTable)
 
   const taskToShow = isHideTasks ? tasks.filter( t => !t.status) : tasks
   const tasksCoordinate = taskToShow.map(task => task.coordinates)
 
+  const marker = new Feature({
+    geometry: new Point(transform([34.4,32], 'EPSG:4326', 'EPSG:3857'))
+  })
+  
+  const markersSourceLayer = new VectoreSource({
+    features: [marker]
+  })
+  const markersLayer = new VectoreLayer({
+    source: markersSourceLayer
+  })
+
+  const basicLayer = new TileLayer({
+    source: new OSM()
+  })
   useEffect(() => {
     new Map({
       controls: new defaultControls({ attribution: false }),
-      layers: [
-        new TileLayer({
-          source: new OSM()
-        })
-      ],
+      layers: [basicLayer,markersLayer],
       target: 'map',
       view: new View({
         center: transform([34.4,32], 'EPSG:4326', 'EPSG:3857'),
         zoom: 7
-      }), 
-
+      }),
     })}, [])
 
   return (
